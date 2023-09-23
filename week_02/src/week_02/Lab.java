f01e36efec44edc94a508c48b67ece246f3b8f63
JEKKY <137982621+vJEKKYv@users.noreply.github.com>
2023-09-24 02:57:33 +0900

    2주차 수정

---
+++ week_02/src/week_02/Lab.java
@@ -0,0 +1,15 @@
+package week_02;
+
+public class Lab {
+	int id;
+	String title;
+	public Lab(int id, String title) {
+		this.id = id;
+		this.title = title;
+	}
+	public int getLabId() {return id;}
+	public String getLabTitle() {return title;}
+	public void ShowData() {
+		System.out.println(id+ "\t"+title);
+	}
+}
--- week_02/src/week_02/LabManager.java
+++ week_02/src/week_02/LabManager.java
@@ -57,16 +57,3 @@ public class LabManager {
 	}
 }
 
-class Lab {
-	int id;
-	String title;
-	public Lab(int id, String title) {
-		this.id = id;
-		this.title = title;
-	}
-	public int getLabId() {return id;}
-	public String getLabTitle() {return title;}
-	public void ShowData() {
-		System.out.println(id+ "\t"+title);
-	}
-}
